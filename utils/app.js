4770b91bc3bf821cd2048750fb33c91a7ae54325
Sahil Kumar <sahil.kumar@datachannel.co>
2024-05-09 20:52:41 +0530

    cors origin policy added

--- utils/app.js
+++ utils/app.js
@@ -6,6 +6,11 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 //  eshop-tutorial-pyri.vercel.app
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+  
 app.use(
     cors({
         origin: "http://localhost:3000",
