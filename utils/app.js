afe4e8e20b526f5dd455bde57cb22c736fa98edb
Sahil Kumar <sahil.kumar@datachannel.co>
2024-05-10 14:08:19 +0530

    cors chaged to open

--- utils/app.js
+++ utils/app.js
@@ -6,17 +6,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 //  eshop-tutorial-pyri.vercel.app
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+//   });
   
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-    })
-);
+// Enable CORS
+app.use(cors({
+    origin: "*",
+    credentials: true,
+}));
 // Increase maximum file size limit
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
