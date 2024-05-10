1c1b82ac23294f3fcd37a36df7ba9319d38923de
Sahil Kumar <sahil.kumar@datachannel.co>
2024-05-10 13:41:54 +0530

    cors changed again

--- utils/app.js
+++ utils/app.js
@@ -7,13 +7,17 @@ const cors = require("cors");
 
 //  eshop-tutorial-pyri.vercel.app
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://aquamarine-sprite-4640e0.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
     next();
-  });
+});
+
   
 app.use(
     cors({
-        origin: "https://aquamarine-sprite-4640e0.netlify.app/",
+        origin: "https://aquamarine-sprite-4640e0.netlify.app",
         credentials: true,
     })
 );
